d776310094b2c57018c3ef4d44d23000f3dbd2f2
Albin <Albin@DESKTOP-2LCI1U3>
2023-07-19 14:57:49 +0200

    lil update

--- Bubblesort/Bubblesort.cpp
+++ Bubblesort/Bubblesort.cpp
@@ -1,40 +1,77 @@
 #include <iostream>
 #include <random>
 #include <vector>
-#include <algorithm>
+int minNumber;
+int maxNumber;
+int amount;
 
-int randomNumber;
-std::string ans;
+void Questiongrid(int minNumber, int maxNumber, int amount) {
+    system("CLS");
+
+    std::cout << "                _________________                _________________                _________________\n";
+    std::cout << "                From: " << minNumber;
+    std::cout << "                          Till: " << maxNumber;
+    std::cout << "                          Amount: " << amount << "\n";
+
+}
 
 void randomnumbergenerator(std::vector<int>& saves) {
-    for (int i = 0; i < 50; i++) {
-        std::random_device rd;
-        std::mt19937 generator(rd());
-        int minNumber = 1;
-        int maxNumber = 1000;
-        std::uniform_int_distribution<int> distribution(minNumber, maxNumber);
-        randomNumber = distribution(generator);
+
+    Questiongrid(minNumber, maxNumber, amount);
+    std::cin >> minNumber;
+    Questiongrid(minNumber, maxNumber, amount);
+    std::cin >> maxNumber;
+    Questiongrid(minNumber, maxNumber, amount);
+    std::cin >> amount;
+    Questiongrid(minNumber, maxNumber, amount);
+
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<int> distribution(minNumber, maxNumber);
+    std::cout << "\n\n";
+    for (int i = 0; i < amount; i++) {
+        int randomNumber = distribution(generator);
         saves.push_back(randomNumber);
-        std::cout << saves[i] << ",";
+        std::cout << saves[i] << " ";
+    }
+    std::cout << "\n------------------------------------------------------------------------------------------------------------------\n";
+
+}
+
+void bubbleSort(std::vector<int>& saves) {
+    int size = saves.size();
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (saves[j] > saves[j + 1]) {
+                int temp = saves[j];
+                saves[j] = saves[j + 1];
+                saves[j + 1] = temp;
+            
+            }
+        }
     }
 }
 
-int main()
-{
+int main() {
     std::vector<int> saves;
 
     do {
         randomnumbergenerator(saves);
-        std::cout << "\n\nready to sort? [Yes/No]\n";
+        std::cout << "\n\nready to sort? [Yes/No]\n\n";
+        std::string ans;
         std::cin >> ans;
 
         if (ans == "Yes") {
-            std::sort(saves.begin(), saves.end());
+            bubbleSort(saves);
             break;
         }
     } while (true);
 
-    for (const auto& num : saves) {
-        std::cout << num << " ";
+    int size = saves.size();
+    std::cout << "\n------------------------------------------------------------------------------------------------------------------\n";
+    for (int i = 0; i < size; i++) {
+        std::cout << saves[i] << " ";
     }
+    std::cout << "\n";
+
 }
